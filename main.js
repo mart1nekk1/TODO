561eb3aeba1fd2c4dca0ce570fa152ddb1efffdd
mart1nekk1 <martin.klobasa73@gmail.com>
2023-12-28 20:27:28 +0100

    Create basic js

---
+++ main.js
@@ -0,0 +1,13 @@
+const inputBox = document.getElementById("input-box")
+const list = document.getElementById("list")
+
+function addTodo() {
+    if(inputBox.value == "") {
+        alert("You must write something!")
+    } else {
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value
+        list.appendChild(li)
+    }
+    inputBox.value = ""
+}
