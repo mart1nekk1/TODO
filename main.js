1b76b9b24fa8a4cd625397a7e912cdb873fa0302
mart1nekk1 <martin.klobasa73@gmail.com>
2023-12-29 16:56:01 +0100

    Remove and check function

--- main.js
+++ main.js
@@ -8,6 +8,18 @@ function addTodo() {
         let li = document.createElement("li")
         li.innerHTML = inputBox.value
         list.appendChild(li)
+
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
     }
     inputBox.value = ""
 }
+
+list.addEventListener("click", function(e) {
+    if(e.target.tagName == "LI") {
+        e.target.classList.toggle("checked")
+    } else if(e.target.tagName == "SPAN") {
+        e.target.parentElement.remove()
+    }
+}, false)
